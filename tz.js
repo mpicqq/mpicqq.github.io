088f168b366f2572c31a9b004e679dda8f49fa8b
mpicqq <303090101@qq.com>
2024-07-11 17:50:16 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -8,8 +8,9 @@ const customParam = urlParams.get('url');
 // 生成随机数字和英文组合 Fr0iy7UeDNxlynjswdEHIcalaTkv
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
-// 构建跳转链接
+// 构建跳转链接  https://p1.meiyintutu.com/s/27qq;3ssm7xj.html
 let redirectURL = "https://p1.meiyintutu.com/s/278v2sssm7xj.html";
+
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
